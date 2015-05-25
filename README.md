80af0b67457c111d5f3c9dae04693310fb717951
Zak B. Elep <zakame@zakame.net>
2015-05-25 09:59:11 +0800

    Initial commit
    This is a backend for Minion job queue, using Amazon Simple Queue
    Service.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Minion::Backend::SQS - It's new $module
+
+# SYNOPSIS
+
+    use Minion::Backend::SQS;
+
+# DESCRIPTION
+
+Minion::Backend::SQS is ...
+
+# LICENSE
+
+Copyright (C) Zak B. Elep.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Zak B. Elep <zakame@cpan.org>
---
+++ lib/Minion/Backend/SQS.pm
@@ -0,0 +1,39 @@
+package Minion::Backend::SQS;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Minion::Backend::SQS - It's new $module
+
+=head1 SYNOPSIS
+
+    use Minion::Backend::SQS;
+
+=head1 DESCRIPTION
+
+Minion::Backend::SQS is ...
+
+=head1 LICENSE
+
+Copyright (C) Zak B. Elep.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Zak B. Elep E<lt>zakame@cpan.orgE<gt>
+
+=cut
+
